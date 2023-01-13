c70405087f2fc977da76986990ed426ce728abdb
Hugues Maignol <hugues@kime.tech>
2023-01-13 17:49:36 +0100

    feat: Prisma decimal serialization

--- apps/web/src/utils/serialization.ts
+++ apps/web/src/utils/serialization.ts
@@ -1,6 +1,17 @@
 import superjson from 'superjson'
 import { SuperJSONResult } from 'superjson/dist/types'
 
+import { Decimal } from 'decimal.js'
+
+superjson.registerCustom<Decimal, string>(
+  {
+    isApplicable: (v): v is Decimal => Decimal.isDecimal(v),
+    serialize: (v) => v.toJSON(),
+    deserialize: (v) => new Decimal(v),
+  },
+  'decimal.js',
+)
+
 // Helper type to obfuscate serialized data
 export type Serialized<T> = { __serialized: Symbol }
 
